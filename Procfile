60471af90841e7ef906e620a01c558be4390bb76
Jon Bills <jon@morphic.software>
2017-06-19 16:18:09 +0100

    Remove ONS_ENV from Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ONS_ENV=int python3 -m swagger_server
+web: python3 -m swagger_server
--- swagger_server/test_local/test_util.py
+++ swagger_server/test_local/test_util.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from swagger_server.controllers_local.util import flatten_keys
-import collections
+
 
 class TestUtil(TestCase):
 
@@ -17,7 +17,4 @@ class TestUtil(TestCase):
 
         expected = ['name', 'other', 'other.name']
 
-        #self.assertEqual(actual, expected)
-
-        # Flatten doesn't guarantee order in resulting list ..
-        self.assertTrue(collections.Counter(actual) == collections.Counter(expected))
+        self.assertCountEqual(actual, expected)
