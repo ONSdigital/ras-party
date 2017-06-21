a517fb6f6850bccb412986ac411a960a40f47e46
Jon Bills <jon@morphic.software>
2017-06-21 16:09:11 +0100

    Validate sampleUnitRef for business parties

--- swagger_server/controllers_local/controller.py
+++ swagger_server/controllers_local/controller.py
@@ -81,6 +81,8 @@ def parties_post(party):
     v = Validator(Exists('sampleUnitType'), IsIn('sampleUnitType', 'B', 'BI'))
     if 'id' in party:
         v.add_rule(IsUuid('id'))
+    if party['sampleUnitType'] == Business.UNIT_TYPE:
+        v.add_rule(Exists('sampleUnitRef'))
     if not v.validate(party):
         return make_response(jsonify(v.errors), 400)
 
