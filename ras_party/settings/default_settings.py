0d89ed110076d5a214fb43fc3ea45c94b32c7dd0
Jon Bills <jon@morphic.software>
2017-07-07 17:05:39 +0100

    Update requirements. Reintroduce CORS

--- ras_party/settings/default_settings.py
+++ ras_party/settings/default_settings.py
@@ -8,5 +8,5 @@ class Config:
 
     # TODO: eventually eliminate ENVIRONMENT_NAME
     ENVIRONMENT_NAME = 'test'
-    # TODO: unify the yaml config with Flask config, e.g. configure Flask from yaml
+    # TODO: unify the yaml config with Flask config, e.g. configure Flask from yaml (consider Flask-Environments)
     CONFIG_PATH = './config.yaml'
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from ons_ras_common.ras_config import ras_config
 from ons_ras_common.ras_database.ras_database import RasDatabase
 
@@ -9,14 +10,14 @@ def create_app(settings_class):
 
     config = ras_config.from_yaml_file(app.config['ENVIRONMENT_NAME'], app.config['CONFIG_PATH'])
     PartyDatabase = RasDatabase.make(model_paths=['swagger_server.models.models'])
-    db = PartyDatabase('ras-party-db', config).activate()
+    db = PartyDatabase('ras-party-db', config)
     app.db = db
     # TODO: investigate a way to unify the environment/config with Flask config, or at least a neater approach!
     app.environment = config
 
     from ras_party.views.party_view import party_view
     app.register_blueprint(party_view, url_prefix='/party-api/v1')
-
+    CORS(app)
     return app
 
 
