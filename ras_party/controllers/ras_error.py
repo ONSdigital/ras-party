38aab413b6ba3ec42b9dec406559afbc47e9bd5e
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2017-07-20 15:10:50 +0100

    Adding setting of user to active on the oauth2 server for ras party service.

--- ras_party/controllers/ras_error.py
+++ ras_party/controllers/ras_error.py
@@ -1,6 +1,8 @@
 
 class RasError(Exception):
     status_code = 500
+    #TODO A RasError will need to deal with more than just a HTTP 500. e.g. Party Service registers a user who has a duplicate
+    # user id. It will have to convey this to the calling party (ras_frontstage) so that it can deal correclty with this issue.
 
     def __init__(self, errors, status_code=None):
         self.errors = errors if type(errors) is list else [errors]
