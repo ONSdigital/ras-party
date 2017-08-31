4ea1a4fd9f9a70408a5b06a9308ce2a1f5113c72
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2017-08-31 16:39:37 +0100

    First commit for fixing the email verficication URL

--- ras_party/controllers/controller.py
+++ ras_party/controllers/controller.py
@@ -589,8 +589,8 @@ def _create_verification_url(email):
     email_token_salt = current_app.config["EMAIL_TOKEN_SALT"] or 'email-confirm-key'
     timed_serializer = URLSafeTimedSerializer(secret_key)
     token = timed_serializer.dumps(email, salt=email_token_salt)
-    frontstage_service = current_app.config.dependency['frontstage-service']
-    verification_url = build_url('{}://{}:{}/register/activate-account/{}', frontstage_service, token)
+    public_email_verification_url = current_app.config["PUBLIC_EMAIL_VERIFICATION_URL"]
+    verification_url = build_url('{}://{}:{}/register/activate-account/{}', public_email_verification_url, token)
 
     return verification_url
 
