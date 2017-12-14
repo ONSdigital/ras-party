a3aace77ae9e7aced7735347b0a63bf9b2776c8f
Jamie Cox <me@jcox.tech>
2017-12-14 15:07:35 +0000

    Simplify public website URL builder

--- ras_party/support/public_website.py
+++ ras_party/support/public_website.py
@@ -5,35 +5,13 @@ class PublicWebsite:
 
     def __init__(self, config):
         self.config = config
-        self.website_url = config['RAS_PUBLIC_WEBSITE_URL']
-
-    @property
-    def scheme(self):
-        return self.config['RAS_PUBLIC_WEBSITE_PROTOCOL']
-
-    @property
-    def host(self):
-        return self.config['RAS_PUBLIC_WEBSITE_HOST']
-
-    @property
-    def port(self):
-        return int(self.config['RAS_PUBLIC_WEBSITE_PORT'])
-
-    @property
-    def host_port(self):
-        if self.scheme == 'https' and self.port == 443:
-            result = self.host
-        elif self.scheme == 'http' and self.port == 80:
-            result = self.host
-        else:
-            result = f'{self.host}:{self.port}'
-        return result
+        self.website_uri = config['RAS_PUBLIC_WEBSITE_URL']
 
     def reset_password_url(self, email):
-        return f'{self.scheme}://{self.host_port}/passwords/reset-password/{self._generate_token(email)}'
+        return f'{self.website_uri}/passwords/reset-password/{self._generate_token(email)}'
 
     def activate_account_url(self, email):
-        return f'{self.scheme}://{self.host_port}/register/activate-account/{self._generate_token(email)}'
+        return f'{self.website_uri}/register/activate-account/{self._generate_token(email)}'
 
     def _generate_token(self, email):
         return generate_email_token(email, self.config)
--- test/support/test_public_website.py
+++ test/support/test_public_website.py
@@ -14,14 +14,24 @@ class MockConfig:
         return {
             'SECRET_KEY': 'secret',
             'EMAIL_TOKEN_SALT': 'salt',
-            'RAS_PUBLIC_WEBSITE_PROTOCOL': self.scheme,
-            'RAS_PUBLIC_WEBSITE_HOST': self.host,
-            'RAS_PUBLIC_WEBSITE_PORT': self.port,
             'RAS_PUBLIC_WEBSITE_URL': f'{self.scheme}://{self.host}:{self.port}',
         }[item]
 
 
 class TestPublicWebsite(TestCase):
+
+    def test_reset_password_url_includes_standard_port_for_http(self):
+        # Given config contains SCHEME 'http and PORT 1234
+        config = MockConfig(scheme='http', port=80)
+        unit = PublicWebsite(config)
+
+        # When the reset password url is constructed
+        actual_url = unit.reset_password_url('test@email.com')
+
+        # Then it contains the port number
+        expected_url_substring = "http://mockhost:80/passwords/reset-password/"
+        # TODO: ought to be possible to mock the token generator, so we can predict the entire url
+        self.assertIn(expected_url_substring, actual_url)
 
     def test_reset_password_url_includes_nonstandard_port_for_http(self):
         # Given config contains SCHEME 'http and PORT 1234
@@ -36,52 +46,17 @@ class TestPublicWebsite(TestCase):
         # TODO: ought to be possible to mock the token generator, so we can predict the entire url
         self.assertIn(expected_url_substring, actual_url)
 
-    def test_reset_password_url_excludes_int_port_80_when_scheme_is_http(self):
-        # Given config contains SCHEME 'http' and PORT 80
-        config = MockConfig(scheme='http', port=80)
-        unit = PublicWebsite(config)
-
-        # When the reset password url is constructed
-        actual_url = unit.reset_password_url('test@email.com')
-
-        expected_url_substring = "http://mockhost/passwords/reset-password/"
-        # Then it does not contain the port number
-        self.assertIn(expected_url_substring, actual_url)
-
-    def test_reset_password_url_excludes_string_port_80_when_scheme_is_http(self):
-        # Given config contains SCHEME 'http' and PORT '80' (in string format)
-        config = MockConfig(port='80')
-        unit = PublicWebsite(config)
-
-        # When the reset password url is constructed
-        actual_url = unit.reset_password_url('test@email.com')
-
-        # Then it does not contain the port number
-        expected_url_substring = "http://mockhost/passwords/reset-password/"
-        self.assertIn(expected_url_substring, actual_url)
-
-    def test_reset_password_url_excludes_int_port_443_when_scheme_is_https(self):
-        # Given config contains SCHEME 'https' and PORT 443
+    def test_reset_password_url_includes_standard_port_for_https(self):
+        # Given config contains SCHEME 'http and PORT 1234
         config = MockConfig(scheme='https', port=443)
         unit = PublicWebsite(config)
 
         # When the reset password url is constructed
         actual_url = unit.reset_password_url('test@email.com')
 
-        # Then it does not contain the port number
-        expected_url_substring = "https://mockhost/passwords/reset-password/"
-        self.assertIn(expected_url_substring, actual_url)
-
-    def test_reset_password_url_excludes_string_port_443_when_scheme_is_https(self):
-        # Given config contains SCHEME 'https' and PORT '443' (in string format)
-        config = MockConfig(scheme='https', port='443')
-        unit = PublicWebsite(config)
-
-        # When the reset password url is constructed
-        actual_url = unit.reset_password_url('test@email.com')
-
-        # Then it does not contain the port number
-        expected_url_substring = "https://mockhost/passwords/reset-password/"
+        # Then it contains the port number
+        expected_url_substring = "https://mockhost:443/passwords/reset-password/"
+        # TODO: ought to be possible to mock the token generator, so we can predict the entire url
         self.assertIn(expected_url_substring, actual_url)
 
     def test_reset_password_url_includes_port_80_when_scheme_is_https(self):
@@ -106,54 +81,6 @@ class TestPublicWebsite(TestCase):
 
         # Then it contains the port number
         expected_url_substring = "http://mockhost:1234/register/activate-account/"
-        self.assertIn(expected_url_substring, actual_url)
-
-    def test_activate_account_url_excludes_int_port_80_when_scheme_is_http(self):
-        # Given config contains SCHEME 'http' and PORT 80
-        config = MockConfig(scheme='http', port=80)
-        unit = PublicWebsite(config)
-
-        # When the activate account url is constructed
-        actual_url = unit.activate_account_url('test@email.com')
-
-        # Then it does not contain the port number
-        expected_url_substring = "http://mockhost/register/activate-account/"
-        self.assertIn(expected_url_substring, actual_url)
-
-    def test_activate_account_url_excludes_string_port_80_when_scheme_is_http(self):
-        # Given config contains SCHEME 'http' and PORT '80' (in string format)
-        config = MockConfig(scheme='http', port='80')
-        unit = PublicWebsite(config)
-
-        # When the activate account url is constructed
-        actual_url = unit.activate_account_url('test@email.com')
-
-        # Then it does not contain the port number
-        expected_url_substring = "http://mockhost/register/activate-account/"
-        self.assertIn(expected_url_substring, actual_url)
-
-    def test_activate_account_url_excludes_int_port_443_when_scheme_is_https(self):
-        # Given config contains SCHEME 'https' and PORT 443
-        config = MockConfig(scheme='https', port=443)
-        unit = PublicWebsite(config)
-
-        # When the activate account url is constructed
-        actual_url = unit.activate_account_url('test@email.com')
-
-        # Then it does not contain the port number
-        expected_url_substring = "https://mockhost/register/activate-account/"
-        self.assertIn(expected_url_substring, actual_url)
-
-    def test_activate_account_url_excludes_string_port_443_when_scheme_is_https(self):
-        # Given config contains SCHEME 'https' and PORT '443' (in string format)
-        config = MockConfig(scheme='https', port='443')
-        unit = PublicWebsite(config)
-
-        # When the activate account url is constructed
-        actual_url = unit.activate_account_url('test@email.com')
-
-        # Then it does not contain the port number
-        expected_url_substring = "https://mockhost/register/activate-account/"
         self.assertIn(expected_url_substring, actual_url)
 
     def test_activate_account_url_includes_port_80_when_scheme_is_https(self):
