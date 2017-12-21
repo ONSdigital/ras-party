d3ddf77cd9b3a2bc2cfd7e03a98438c72c510727
Jamie Cox <me@jcox.tech>
2017-12-21 10:00:32 +0000

    Remove whitespace

--- test/test_config.py
+++ test/test_config.py
@@ -4,14 +4,14 @@ from config import _is_true
 
 
 class TestConfig(TestCase):
-    
+
     def test_falsey_config(self):
         self.assertFalse(_is_true(False))
         self.assertFalse(_is_true('False'))
         self.assertFalse(_is_true(None))
         self.assertFalse(_is_true('x'))
         self.assertFalse(_is_true('0'))
-    
+
     def test_truthy_config(self):
         self.assertTrue(_is_true(True))
         self.assertTrue(_is_true('true'))
