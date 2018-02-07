d2b40508fcf6b3303c5c3c2b0776d460c3aaf4c5
Tejas Patel <tejas.patel@ons.gov.uk>
2018-02-07 16:20:14 +0000

    remove unused import

--- ras_party/views/business_view.py
+++ ras_party/views/business_view.py
@@ -2,7 +2,6 @@ from flask import Blueprint, request, make_response, current_app, jsonify
 from flask_httpauth import HTTPBasicAuth
 
 from ras_party.controllers import business_controller
-from ras_party.exceptions import RasError
 from ras_party.support.log_decorator import log_route
 
 
