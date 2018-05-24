e7661724f8a36213826de54194064f75cdb4de94
rhiGriff <rhiannongriffiths73@gmail.com>
2018-05-24 15:24:08 +0100

    fixing log line for splunk (#135)
    * Log exceptions in error handler
    
    We should be catching and handling exceptions at the source where
    applicable to resolve them in the best way. Where uncaught exceptions
    slip through we weren't logging the traceback. This makes it difficult
    to track the cause of the issue.
    
    The error handler has been changed to log all tracebacks.

---
+++ test/test_session_decorator.py
@@ -0,0 +1,46 @@
+import unittest
+from unittest.mock import patch, Mock
+
+from ras_party.exceptions import RasError
+from ras_party.support.session_decorator import handle_session
+
+
+class TestSessionDecorator(unittest.TestCase):
+
+    def test_session_decorator_removes_session(self):
+        # Given
+        def do_nothing(session): pass
+        session_instance = Mock()
+        # When
+        with patch('ras_party.support.session_decorator.current_app') as current_app:
+            current_app.db.session.return_value = session_instance
+            handle_session(do_nothing, [], {})
+
+            # Then
+            current_app.db.session.remove.assert_called_once()
+
+    def test_session_decorator_rollback_exception(self):
+        # Given
+        def raise_exception(session): raise Exception
+        session_instance = Mock()
+        # When
+        with patch('ras_party.support.session_decorator.current_app') as current_app:
+            current_app.db.session.return_value = session_instance
+            self.assertRaises(Exception, handle_session, raise_exception, [], {})
+
+            # Then
+            session_instance.rollback.assert_called_once()
+            current_app.db.session.remove.assert_called_once()
+
+    def test_session_decorator_rollback_ras_error(self):
+        # Given
+        def raise_exception(session): raise RasError(None)
+        session_instance = Mock()
+        # When
+        with patch('ras_party.support.session_decorator.current_app') as current_app:
+            current_app.db.session.return_value = session_instance
+            self.assertRaises(RasError, handle_session, raise_exception, [], {})
+
+            # Then
+            session_instance.rollback.assert_called_once()
+            current_app.db.session.remove.assert_called_once()
