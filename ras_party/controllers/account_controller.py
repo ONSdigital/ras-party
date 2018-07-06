c5f6034e4c9e13c2cd5ac719214a929d76ed2feb
Gavin Edwards <gavin.edwards@ons.gsi.gov.uk>
2018-07-06 09:42:31 +0100

    Check for null value as well as empty string

--- ras_party/controllers/account_controller.py
+++ ras_party/controllers/account_controller.py
@@ -410,7 +410,7 @@ def resend_verification_email(party_uuid, session):
     if not respondent:
         raise RasError(NO_RESPONDENT_FOR_PARTY_ID, status=404)
 
-    if respondent.pending_email_address != '':
+    if respondent.pending_email_address:
         _send_email_verification(party_uuid, respondent.pending_email_address)
     else:
         _send_email_verification(party_uuid, respondent.email_address)
