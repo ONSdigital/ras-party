215ada0ae2baca0e31ebad12f2b2fe3022b97f7e
Nick Grant <nick.grant@ext.ons.gov.uk>
2018-11-29 17:09:35 +0000

    Increase pool size

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class Config(object):
     EMAIL_TOKEN_EXPIRY = int(os.getenv('EMAIL_TOKEN_EXPIRY', 306000))
     PARTY_SCHEMA = os.getenv('PARTY_SCHEMA', 'ras_party/schemas/party_schema.json')
 
-    DB_POOL_SIZE = int(os.getenv('DB_POOL_SIZE', 5))
+    DB_POOL_SIZE = int(os.getenv('DB_POOL_SIZE', 15))
     DB_MAX_OVERFLOW = int(os.getenv('DB_MAX_OVERFLOW', 10))
     DB_POOL_RECYCLE = int(os.getenv('DB_POOL_RECYCLE', -1))
 
