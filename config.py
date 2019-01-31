29447b02a767847aab46ef9d588030e70583ccae
ryangrundy7 <35301276+ryangrundy7@users.noreply.github.com>
2019-01-31 12:41:32 +0000

    Update version (#190)

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ def _is_true(value):
 class Config(object):
 
     NAME = os.getenv('RAS-PARTY', 'ras-party')
-    VERSION = os.getenv('VERSION', '1.4.0')
+    VERSION = os.getenv('VERSION', '1.6.2')
     SCHEME = os.getenv('http')
     HOST = os.getenv('HOST', '0.0.0.0')
     PORT = os.getenv('PORT', 8081)
