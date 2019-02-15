efb4f1b158d988e61f7680940a5e886a01149906
Andrew Torrance <Andrew.Torrance@Ons.Gov.uk>
2019-02-15 16:35:37 +0000

    put old migration scripts back for the moment. Added change to not run migrations to line up with bug fix added script that can be run instead of upgrade script for respondent indexes.

--- migrations/versions/0de421d5c9bf_new_indexes_for_respondent_search.py
+++ migrations/versions/0de421d5c9bf_new_indexes_for_respondent_search.py
@@ -1,7 +1,7 @@
 """new indexes for respondent search
 
 Revision ID: 0de421d5c9bf
-Revises: fa52b1008d60
+Revises: 613bf3a40948
 Create Date: 2019-02-13 11:33:50.433506
 
 """
@@ -9,18 +9,21 @@ from migrations.migration_utilities import try_add_index, try_remove_index
 
 # revision identifiers, used by Alembic.
 revision = '0de421d5c9bf'
-down_revision = 'fa52b1008d60'
+down_revision = '613bf3a40948'
 branch_labels = None
 depends_on = None
 
+# upgrade and downgrade left in atm as I dont know at what state db / alembic/ redis will be at release time
 
 def upgrade():
+    return
     _try_add_party_respondent_index('respondent_first_name_idx', ['first_name'])
     _try_add_party_respondent_index('respondent_last_name_idx', ['last_name'])
     _try_add_party_respondent_index('respondent_email_idx', ['email_address'])
 
 
 def downgrade():
+    return
     _try_remove_party_respondent_index('respondent_first_name_idx')
     _try_remove_party_respondent_index('respondent_last_name_idx')
     _try_remove_party_respondent_index('respondent_email_idx')
---
+++ migrations/versions/ac604d2cf763_add_pending_email_address_to_respondent.py
@@ -0,0 +1,28 @@
+"""add pending email address to respondent
+
+Revision ID: ac604d2cf763
+Revises: dfeba9ccfc46
+Create Date: 2018-05-01 09:39:19.452818
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = 'ac604d2cf763'
+down_revision = 'dfeba9ccfc46'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        'respondent',
+        sa.Column('pending_email_address', sa.Text, nullable=True),
+        schema='partysvc'
+    )
+
+
+def downgrade():
+    op.drop_column('respondent', 'pending_email_address', schema='partysvc')
