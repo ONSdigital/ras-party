56a7ef859a0776a5bbbe00829d264e51632841fe
Andrew Torrance <Andrew.Torrance@Ons.Gov.uk>
2019-02-19 10:05:46 +0000

    Remove unnecessary comments

--- test/test_create_database.py
+++ test/test_create_database.py
@@ -15,15 +15,8 @@ class TestCreateDatabase(TestCase):
         db_connection = 'postgres://postgres:postgres@localhost:5432/postgres'
         db_schema = 'partysvc'
 
-        # START 15-Feb-2019 Bug Fix 751
-        # with patch('run.create_engine'), patch('run.scoped_session'), patch('run.command.upgrade') as upgrade:
-        #     create_database(db_connection, db_schema, 5, 10, -1)
-        # See also run.py
-
         with patch('run.create_engine'), patch('run.scoped_session') as upgrade:
             create_database(db_connection, db_schema, 5, 10, -1)
-
-        # END 15-Feb-2019 Bug Fix 751
 
         upgrade.assert_called_once()
 
