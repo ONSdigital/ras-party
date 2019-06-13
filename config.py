0e4fa7d01d1e06165ca4c3bde47684330f3904c5
mfcrocker <matthew.crocker@ons.gov.uk>
2019-06-13 10:31:52 +0100

    Update version to 1.7.2 (#197)

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ def _is_true(value):
 class Config(object):
 
     NAME = os.getenv('RAS-PARTY', 'ras-party')
-    VERSION = os.getenv('VERSION', '1.7.1')
+    VERSION = os.getenv('VERSION', '1.7.2')
     SCHEME = os.getenv('http')
     HOST = os.getenv('HOST', '0.0.0.0')
     PORT = os.getenv('PORT', 8081)
