932e51611c67c9797973d9cbcd6a9de5c6b22eb9
Andrew <andrew.torrance@ons.gov.uk>
2019-10-29 13:56:04 +0000

    tidied up script

--- scripts/migrate_name_and_trading_as.py
+++ scripts/migrate_name_and_trading_as.py
@@ -5,6 +5,7 @@ So that search speed can be improved
 """
 import sys
 import os
+import time
 
 parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir_path)
@@ -19,30 +20,40 @@ def migrate_name_and_trading_as():
     last_id = first_id + batch_size
 
     maximum_id = _get_maximum_id()
+    print(f"{maximum_id} records to update")
+    error = None
 
-    while first_id <= maximum_id:
+    while first_id <= maximum_id and not error:
+        try:
+            print(f"first_id={first_id} , last_id={last_id} batch_size={batch_size}")
 
-        connection = psycopg2.connect(Config.DATABASE_URI)
-        cursor = connection.cursor()
+            connection = psycopg2.connect(Config.DATABASE_URI)
+            cursor = connection.cursor()
 
-        sql_query = f"SELECT * FROM partysvc.business_attributes WHERE id > {first_id} AND id < {last_id}"
-        cursor.execute(sql_query)
-        result = cursor.fetchall()
-        for row in result:
+            sql_query = f"SELECT * FROM partysvc.business_attributes WHERE id > {first_id} AND id < {last_id}"
+            cursor.execute(sql_query)
+            result = cursor.fetchall()
+            for row in result:
+                # named columns not returned so use positional
+                trading_as = row[4].get('trading_as', '')
 
-            # named columns not returned so use positional
-            trading_as = row[4].get('trading_as', '')
+                name = row[4].get('name', '')
 
-            name = row[4].get('name', '')
+                insert_sql = f"UPDATE partysvc.business_attributes " \
+                             f"SET trading_as = '{trading_as}', name = '{name}'" \
+                             f" WHERE id={row[0]}"
+                cursor.execute(insert_sql)
 
-            insert_sql = f"UPDATE partysvc.business_attributes " \
-                         f"SET trading_as = '{trading_as}', name = '{name}'" \
-                         f" WHERE id={row[0]}"
-            cursor.execute(insert_sql)
+            first_id = last_id
+            last_id += batch_size
+            connection.commit()
 
-        first_id = last_id
-        last_id += batch_size
-        connection.commit()
+        except(Exception, psycopg2.Error) as error:
+            print("Failed updating business attributes", error)
+        finally:
+            cursor.close()
+            connection.close()
+            print(f"script complete, connections closed")
 
 
 def _get_maximum_id():
