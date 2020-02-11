f5f8a2152df4cc0ffcd6aecf38bf9abf52ceff04
jdfox <jdfox@users.noreply.github.com>
2020-02-11 11:03:00 +0000

    Remove email from logging (#230)
    * Add fix.  Tests failing
    
    * added handling of malformed addresses and a unit test
    
    * simplified obfuscate_email in util.py
    
    Co-authored-by: Adam Mann <manny160389@hotmail.com>
    Co-authored-by: jterryons <57897360+jterryons@users.noreply.github.com>

--- ras_party/support/util.py
+++ ras_party/support/util.py
@@ -64,7 +64,17 @@ def obfuscate_email(email):
     """Takes an email address and returns an obfuscated version of it.
     For example: test@example.com would turn into t**t@e*********m
     """
-    m = email.split('@')
-    prefix = f'{m[0][0]}{"*"*(len(m[0])-2)}{m[0][-1]}'
-    domain = f'{m[1][0]}{"*"*(len(m[1])-2)}{m[1][-1]}'
-    return f'{prefix}@{domain}'
+    if email is None:
+        return None
+    splitmail = email.split('@')
+    # If the prefix is 1 character, then we can't obfuscate it
+    if len(splitmail[0]) <= 1:
+        prefix = splitmail[0]
+    else:
+        prefix = f'{splitmail[0][0]}{"*"*(len(splitmail[0])-2)}{splitmail[0][-1]}'
+    # If the domain is missing or 1 character, then we can't obfuscate it
+    if len(splitmail) <= 1 or len(splitmail[1]) <= 1:
+        return f'{prefix}'
+    else:
+        domain = f'{splitmail[1][0]}{"*"*(len(splitmail[1])-2)}{splitmail[1][-1]}'
+        return f'{prefix}@{domain}'
--- test/test_util.py
+++ test/test_util.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from ras_party.support.util import flatten_keys
+from ras_party.support.util import flatten_keys, obfuscate_email
 
 
 class TestUtil(TestCase):
@@ -18,3 +18,23 @@ class TestUtil(TestCase):
         expected = ['name', 'other', 'other.name']
 
         self.assertCountEqual(actual, expected)
+
+    def test_obfuscate_email(self):
+        """Test obfuscate_email correctly changes inputted emails"""
+
+        testAddresses = {
+            "example@example.com": "e*****e@e*********m",
+            "prefix@domain.co.uk": "p****x@d**********k",
+            "first.name@place.gov.uk": "f********e@p**********k",
+            "me+addition@gmail.com": "m*********n@g*******m",
+            "a.b.c.someone@example.com": "a***********e@e*********m",
+            "john.smith123456@londinium.ac.co.uk": "j**************6@l****************k",
+            "me!?@example.com": "m**?@e*********m",
+            "m@m.com": "m@m***m",
+            "joe.bloggs": "j********s",
+            "joe.bloggs@": "j********s",
+            "@gmail.com": "@g*******m"
+        }
+
+        for test in testAddresses:
+            self.assertEqual(obfuscate_email(test), testAddresses[test])
