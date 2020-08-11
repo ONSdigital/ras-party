8a1b7937c215138e0def89ba1bbc1f4a611952f4
Adam Mann <manny160389@hotmail.com>
2020-08-11 13:59:51 +0100

    More tidyup

--- ras_party/controllers/notify_gateway.py
+++ ras_party/controllers/notify_gateway.py
@@ -7,6 +7,7 @@ import structlog
 from ras_party.exceptions import RasNotifyError
 from ras_party.support.requests_wrapper import Requests
 from google.cloud import pubsub_v1
+from concurrent.futures import TimeoutError
 
 
 logger = structlog.wrap_logger(logging.getLogger(__name__))
@@ -22,6 +23,8 @@ class NotifyGateway:
         self.request_password_change_template = config['NOTIFY_REQUEST_PASSWORD_CHANGE_TEMPLATE']
         self.confirm_password_change_template = config['NOTIFY_CONFIRM_PASSWORD_CHANGE_TEMPLATE']
         self.notify_account_locked = config['NOTIFY_ACCOUNT_LOCKED_TEMPLATE']
+        self.project_id = self.config['GOOGLE_CLOUD_PROJECT']
+        self.topic_id = self.config['NOTIFY_PUBSUB_TOPIC']
 
     def _send_message(self, email, template_id, personalisation=None, reference=None):
         """
@@ -33,7 +36,7 @@ class NotifyGateway:
         :param reference: reference to be generated if not using Notify's id
         :rtype: 201 if success
         """
-
+        logger.info("Sending email via notify-gateway", template_id=template_id)
         if not self.config['SEND_EMAIL_TO_GOV_NOTIFY']:
             logger.info("Notification not sent. Notify is disabled.")
             return
@@ -65,47 +68,62 @@ class NotifyGateway:
         :type email: str
         :param template_id: A uuid of the template_id to be used in gov notify
         :type template_id: str
-        :param personalisation: A dictionary containing
+        :param personalisation: A dictionary containing variables that will be used in the email e.g., names, ru refs
         :type personalisation: dict
-        :return:
+        :raises RasNotifyError: Raised on any Exception that occurs.  Most likely will happen if there is an issue when
+                                publishing to pubsub.
+        :return: None
         """
-        logger.info("Sending email via pubsub")
+        bound_logger = logger.bind(template_id=template_id, project_id=self.project_id, topic_id=self.topic_id)
+        bound_logger.info("Sending email via pubsub")
         if not self.config['SEND_EMAIL_TO_GOV_NOTIFY']:
             logger.info("Notification not sent. Notify is disabled.")
             return
 
-        try:
-            payload = {
-                'notify': {
-                    'email_address': email,
-                    'template_id': template_id,
-                    'personalisation': {}
-                }
+        payload = {
+            'notify': {
+                'email_address': email,
+                'template_id': template_id,
+                'personalisation': {}
             }
-            if personalisation:
-                payload['notify']['personalisation'] = personalisation
+        }
+        if personalisation:
+            payload['notify']['personalisation'] = personalisation
 
-            payload_str = json.dumps(payload)
-            payload_bytes = payload_str.encode()
+        payload_str = json.dumps(payload)
+        publisher = pubsub_v1.PublisherClient()
+        topic_path = publisher.topic_path(self.project_id, self.topic_id)
 
-            publisher = pubsub_v1.PublisherClient()
-            project_id = self.config['GOOGLE_CLOUD_PROJECT']
-            topic_id = self.config['NOTIFY_PUBSUB_TOPIC']
-            logger.info("things", project_id=project_id, topic_id=topic_id)
-            topic_path = publisher.topic_path(project_id, topic_id)
+        bound_logger.info("About to publish to pubsub")
+        future = publisher.publish(topic_path, data=payload_str.encode())
 
-            # Fix logging after functionality works
-            logger.info("topic path", topic_path=topic_path)
-            logger.info("payload", payload=payload)
-            future = publisher.publish(topic_path, data=payload_bytes)
-            logger.info("Publish result", result=future.result())
-
-        except Exception as e:
-            logger.error("error", error=e)
-            raise RasNotifyError("There was a problem sending a notification to Notify-Gateway to GOV.UK Notify",
-                                 error=e)
+        # It's okay for us to catch a broad Exception here because the documentation for future.result() says it
+        # throws either a TimeoutError or an Exception.
+        try:
+            msg_id = future.result()
+            bound_logger.info("Publish succeeded", msg_id=msg_id)
+        except TimeoutError as e:
+            bound_logger.error("Publish to pubsub timed out", exc_info=True)
+            raise RasNotifyError("Publish to pubsub timed out", error=e)
+        except Exception as e: # noqa
+            bound_logger.error("A non-timeout error was raised when publishing to pubsub", exc_info=True)
+            raise RasNotifyError("A non-timeout error was raised when publishing to pubsub", error=e)
 
     def request_to_notify(self, email, template_name, personalisation=None, reference=None):
+        """
+        Sends a message to either notify-gateway or a pubsub topic which will ultimately result in an email being
+        sent via gov notify
+
+        :param email: Email address to send the email too
+        :type email: str
+        :param template_name: Name of the template.  This will be translated into a uuid for gov notify to use.
+        :type template_name: str
+        :param personalisation: A dictionary containing variables that will be used in the email e.g., names, ru refs
+        :type personalisation: dict
+        :param reference:
+        :raises KeyError: Raised if the template name doesn't have a mapping in this class.
+
+        """
         template_id = self._get_template_id(template_name)
         if self.config['USE_PUBSUB_FOR_EMAIL']:
             self._send_message_via_pubsub(email, template_id, personalisation)
