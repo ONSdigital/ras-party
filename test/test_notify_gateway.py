3417fda44f3743d0fee597033262f73aab306bed
Adam Mann <manny160389@hotmail.com>
2020-08-26 15:30:40 +0100

    Remove unit tests for removed code

--- test/test_notify_gateway.py
+++ test/test_notify_gateway.py
@@ -6,70 +6,7 @@ from flask_testing import TestCase
 
 from ras_party.controllers.notify_gateway import NotifyGateway
 from ras_party.exceptions import RasNotifyError
-from ras_party.support.requests_wrapper import Requests
 from run import create_app
-from test.mocks import MockRequests
-from test.party_client import PartyTestClient
-
-
-class TestNotifyGateway(PartyTestClient):
-    """ Notify gateway test class"""
-    def setUp(self):
-        self.mock_requests = MockRequests()
-        Requests._lib = self.mock_requests
-
-    def test_notify_sends_notification_with_basic_message(self):
-        # Given a mocked notify gateway and response
-        self.mock_requests.post.response_payload = '{"id": "notification id"}'
-
-        notify = NotifyGateway(current_app.config)
-        # When an email is sent
-        notify.request_to_notify('email', 'email_verification')
-        # Then send_email_notification is called
-
-        expected_data = {"emailAddress": "email"}
-
-        expected_request = {"auth": (current_app.config['SECURITY_USER_NAME'],
-                                     current_app.config['SECURITY_USER_PASSWORD']),
-                            "timeout": 20, "json": expected_data}
-
-        self.mock_requests.post.assert_called_with(
-            "http://mockhost:5555/emails/email_verification_id",
-            expected_request)
-
-    def test_notify_sends_notification_with_extended_message(self):
-        # Given a mocked notify gateway and response
-        self.mock_requests.post.response_payload = '{"id": "notification id"}'
-
-        notify = NotifyGateway(current_app.config)
-        # When an email is sent
-        personalisation = {
-            "name": "A person"
-        }
-        notify.request_to_notify("email", 'request_password_change', personalisation=personalisation,
-                                 reference="reference")
-        # Then send_email_notification is called
-        expected_data = {"emailAddress": "email", "personalisation": personalisation, "reference": "reference"}
-        expected_request = {"auth": (current_app.config['SECURITY_USER_NAME'],
-                                     current_app.config['SECURITY_USER_PASSWORD']),
-                            "timeout": 20, "json": expected_data}
-
-        self.mock_requests.post.assert_called_with(
-            "http://mockhost:5555/emails/request_password_change_id",
-            expected_request)
-
-    def test_notify_exception_is_translated_to_ras_exception(self):
-        # Given a mocked gov.uk notify and exception
-        def mock_post_notify(*args, **kwargs):
-            raise Exception("Generic Error")
-
-        self.mock_requests.post = mock_post_notify
-
-        # When an email is sent
-        notify = NotifyGateway(current_app.config)
-        # Then a RasNotifyError is raised
-        with self.assertRaises(RasNotifyError):
-            notify.request_to_notify('email', 'request_password_change')
 
 
 class TestNotifyGatewayUnit(TestCase):
