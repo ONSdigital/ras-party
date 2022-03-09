e94b58ac7391d5da68bc7f9f7478ea4d02f17267
Alejandro Arroyo <alejandro.arroyo@ons.gov.uk>
2022-03-09 09:34:20 +0000

    adding more tests

--- test/controllers/test_account_controller.py
+++ test/controllers/test_account_controller.py
@@ -1,3 +1,4 @@
+import base64
 import datetime
 import json
 import os
@@ -6,6 +7,7 @@ from unittest import TestCase
 from unittest.mock import MagicMock
 
 import responses
+from requests import HTTPError
 from sqlalchemy import and_
 
 from config import TestingConfig
@@ -23,6 +25,7 @@ class TestAccountController(TestCase):
 
     def setUp(self):
         self.app = create_app("TestingConfig")
+        self.client = self.app.test_client()
 
     project_root = os.path.dirname(os.path.dirname(__file__))
     valid_business_party_id = "3b136c4b-7a14-4904-9e01-13364dd7b972"
@@ -30,6 +33,8 @@ class TestAccountController(TestCase):
     valid_case_id = "10b04906-f478-47f9-a985-783400dd8482"
     valid_case_group_id = "612f5c34-7e11-4740-8e24-cb321a86a917"
     valid_respondent_id = "438df969-7c9c-4cd4-a89b-ac88cf0bfdf3"
+    invalid_respondent_id = "invalid id"
+    auth_headers = {"Authorization": "Basic %s" % base64.b64encode(b"username:password").decode("ascii")}
     valid_payload = {
         "respondent_id": valid_respondent_id,
         "business_id": valid_business_party_id,
@@ -122,6 +127,69 @@ class TestAccountController(TestCase):
             rsps.assert_call_count(self.url_request_casegroups_for_business, 1)
             rsps.assert_call_count(self.url_get_cases_for_casegroup, 1)
             rsps.assert_call_count(self.url_change_respondent_enrolment_status, 1)
+
+    def test_change_respondent_enrolment_status_no_respondent(self):
+        no_respondent_payload = {
+            "respondent_id": self.invalid_respondent_id,
+            "business_id": self.valid_business_party_id,
+            "survey_id": self.valid_survey_id,
+            "change_flag": "ENABLED",
+        }
+        with responses.RequestsMock() as rsps:
+            session = MagicMock()
+            session.query(Respondent).filter(Respondent.party_uuid == self.valid_business_party_id).first(
+                1
+            ).return_value = self.get_respondent_object()
+            session.query(Enrolment).filter(
+                and_(
+                    Enrolment.respondent_id == self.valid_respondent_id,
+                    Enrolment.business_id == self.valid_business_party_id,
+                    Enrolment.survey_id == self.valid_survey_id,
+                )
+            ).first(2).return_value = self.get_enrolment_object()
+            session.query().filter().count.return_value = 1
+            rsps.add(rsps.GET, self.url_request_collection_exercises_for_survey, json=self.collex_for_survey)
+            rsps.add(rsps.GET, self.url_request_casegroups_for_business, json=self.business_casegroups)
+            rsps.add(rsps.GET, self.url_get_cases_for_casegroup, json=self.cases_for_casegroup)
+            rsps.add(rsps.POST, self.url_change_respondent_enrolment_status, json={}, status=404)
+            with self.app.app_context():
+                try:
+                    account_controller.change_respondent_enrolment_status.__wrapped__(no_respondent_payload, session)
+                except HTTPError:
+                    rsps.assert_call_count(self.url_request_collection_exercises_for_survey, 1)
+                    rsps.assert_call_count(self.url_request_casegroups_for_business, 1)
+                    rsps.assert_call_count(self.url_get_cases_for_casegroup, 1)
+                    rsps.assert_call_count(self.url_change_respondent_enrolment_status, 1)
+                    self.assertRaises(HTTPError)
+
+    def test_change_respondent_enrolment_status_bad_request(self):
+        bad_request_payload = {
+            "wrong_json": "wrong_json",
+        }
+        with self.app.app_context():
+            response = self.client.put(
+                "/party-api/v1/respondents/change_enrolment_status",
+                headers=self.auth_headers,
+                data=json.dumps(bad_request_payload),
+                content_type="application/vnd.ons.business+json",
+            )
+            self.assertEqual(400, response.status_code)
+
+    def test_change_respondent_enrolment_status_random_string_fail(self):
+        random_string_payload = {
+            "respondent_id": self.valid_respondent_id,
+            "business_id": self.valid_business_party_id,
+            "survey_id": self.valid_survey_id,
+            "change_flag": "woafouewbhouGFHEPIW0",
+        }
+        with self.app.app_context():
+            response = self.client.put(
+                "/party-api/v1/respondents/change_enrolment_status",
+                headers=self.auth_headers,
+                data=json.dumps(random_string_payload),
+                content_type="application/vnd.ons.business+json",
+            )
+            self.assertEqual(500, response.status_code)
 
 
 if __name__ == "__main__":
