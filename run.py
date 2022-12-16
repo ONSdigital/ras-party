f6a10b7788fe9e6d3ad6add674cfc0f4a646f000
SteveScorfield <47788084+SteveScorfield@users.noreply.github.com>
2022-12-16 13:12:50 +0000

    Upgrade flask and werkzeug to latest version in ras-party (#384)
    * Initial commit of code
    
    * Fix failing tests and correct makefile
    
    * Add additional ignore for 42194
    
    * auto patch increment
    
    Co-authored-by: ras-rm-pr-bot <matt.crocker+rasrm@gmail.com>

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 from json import loads
 
 import structlog
-from flask import Flask, _app_ctx_stack
+from flask import Flask
 from flask_cors import CORS
 from retrying import RetryError, retry
 from sqlalchemy import column, create_engine, text
@@ -50,11 +50,8 @@ def create_app(config=None):
 def create_database(db_connection, db_schema):
     from ras_party.models import models
 
-    def current_request():
-        return _app_ctx_stack.__ident_func__()
-
-    engine = create_engine(db_connection)
-    session = scoped_session(sessionmaker(), scopefunc=current_request)
+    engine = create_engine(db_connection, echo=True)
+    session = scoped_session(sessionmaker())
     session.configure(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
     engine.session = session
     models.Base.query = session.query_property()
