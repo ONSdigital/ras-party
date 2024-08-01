9977ada8c2afc459b5a807f02700684bff5cfad3
Alejandro Arroyo <alejandro.arroyo@ons.gov.uk>
2024-08-01 12:58:58 +0100

    updating docker-compose to docker compose

--- party-compose.sh
+++ party-compose.sh
@@ -3,8 +3,8 @@
 ###
 # Creates ras-party and db containers and populates the database with test data
 ###
-docker-compose -f docker-compose.yml down
-docker-compose -f docker-compose.yml up -d
+docker compose -f docker-compose.yml down
+docker compose -f docker-compose.yml up -d
 sleep 10
 docker cp scripts/install_test_data.sql postgres:/install_test_data.sql
 sleep 10
