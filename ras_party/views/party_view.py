d1accd5e7aa8a001767b32a4bd1626d09fb0ddcb
SteveScorfield <steve.scorfield@ons.gov.uk>
2025-04-15 13:36:03 +0100

    Adding some log times in tto test performance

--- ras_party/views/party_view.py
+++ ras_party/views/party_view.py
@@ -1,10 +1,15 @@
+from datetime import datetime
+
 from flask import Blueprint, current_app, jsonify, request
 from flask_httpauth import HTTPBasicAuth
-
+import logging
+import structlog
 from ras_party.controllers import party_controller
 
 party_view = Blueprint("party_view", __name__)
 auth = HTTPBasicAuth()
+
+logger = structlog.wrap_logger(logging.getLogger(__name__))
 
 
 @party_view.before_request
@@ -23,8 +28,10 @@ def get_pw(username):
 
 @party_view.route("/parties", methods=["POST"])
 def post_party():
+    logger.info("Started processing CSV queries at " + str(datetime.now()))
     payload = request.get_json() or {}
     response = party_controller.parties_post(payload)
+    logger.info("Finished processing CSV queries at " + str(datetime.now()))
     return jsonify(response)
 
 
