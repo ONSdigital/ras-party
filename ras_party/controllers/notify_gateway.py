881f2c4a1650b8d96d3858b649a69f9891cd42f5
matthew-robinson-ons <matthew.robinson@ons.gov.uk>
2026-06-24 14:32:50 +0100

    revert

--- ras_party/controllers/notify_gateway.py
+++ ras_party/controllers/notify_gateway.py
@@ -2,13 +2,6 @@ import json
 import logging
 from concurrent.futures import TimeoutError
 
-from gevent import monkey
-monkey.patch_all()
-
-import grpc.experimental.gevent as grpc_gevent
-grpc_gevent.init_gevent()
-
-
 import structlog
 from google.cloud import pubsub_v1
 
