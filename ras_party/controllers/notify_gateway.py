711fb92d509171ec3fb11567d3b75b2e9dee5412
Warren Bailey (ONS) <warren.bailey@ext.ons.gov.uk>
2026-09-16 11:43:45 +0100

    fix formatting

--- ras_party/controllers/notify_gateway.py
+++ ras_party/controllers/notify_gateway.py
@@ -38,7 +38,7 @@ class NotifyGateway:
 
     @classmethod
     def get_publisher(cls):
-         return cls._publisher
+        return cls._publisher
 
     def _send_message(self, email, template_id, personalisation):
         """Sends an email via pubsub topic
